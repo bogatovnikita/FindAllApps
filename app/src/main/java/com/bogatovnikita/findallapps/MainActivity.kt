e8678a87397650117c26badd70fe5588d8485ab0
bogatovnikita <GfiFhj57?,;)&gjJv57!>
2022-01-17 20:16:12 +0300

    Add night themes

--- app/src/main/java/com/bogatovnikita/findallapps/MainActivity.kt
+++ app/src/main/java/com/bogatovnikita/findallapps/MainActivity.kt
@@ -2,10 +2,26 @@ package com.bogatovnikita.findallapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.bogatovnikita.findallapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initSwitchCompat()
+    }
+
+    private fun initSwitchCompat() {
+        binding.switchCompat.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
